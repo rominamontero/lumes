84d03fd01d0152fc7c3dbfebc1fe5050a1878adf
rominamontero <rmontero2208@gmail.com>
2026-05-24 22:08:24 -0400

    ahora esta completo

--- 1805.py
+++ 1805.py
@@ -6,14 +6,16 @@ contM=0
 
 while True:
 
-    nombre=input("Ingrese el nombre del juego: ")
-
     while True:
-        if len(nombre) >= 5 and " " not in nombre and nombre.isupper: 
-            break
+        nombre = input("Ingrese el nombre del juego: ")
+        if len(nombre) < 5:
+            print("Error: el nombre debe tener 5 caracteres como mínimo")
+        elif " " in nombre:
+            print("Error: el nombre no debe tener espacios")
+        elif not nombre.isupper():
+            print("Error: el nombre debe estar en MAYUSCULAS")
         else:
-            print("Error el nombre no debe tener espacios, debe tener 5 caracteres como minimo y debe estar en MAYUSCULAS")
-            nombre=input("Ingrese el nombre del juego: ")
+            break
 
     while True:
         try:
@@ -25,13 +27,13 @@ while True:
         except ValueError:
             print("Debe ingresar solo numeros")
 
-    if precio >= 20000 and precio >= 40000:
+    if precio <= 40000:
         indie+=1
-    elif precio< 40000:
+    elif precio > 40000:
         estudio+=1
 
     while True:
-        clasificacion=input("Ingrese la clasificacion (E (<12), A (+12) o M (+18)): ")
+        clasificacion=input("Ingrese la clasificacion (E (<12), A (+12) o M (+18)): ").upper()
         if clasificacion=="E": 
             cont+=1
             break
@@ -41,8 +43,10 @@ while True:
         elif clasificacion =="M":
             contM+=1
             break
+        else:
+            print("Error: debe ingresar E, A o M")
     print(f"Hay {indie} indies, {estudio} de estudio. Solo {cont} son de clasificacion E, {contA} de clasificacion A y {contM} de clasificacion M")
-    continuar=input("Desea continuar S/N:  ")
+    continuar=input("Desea continuar S/N:  ").upper()
     if continuar=="N":
         break
 
